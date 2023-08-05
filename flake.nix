46a4b85f19cb08f1c2146dac892b2220a93809ba
pegasust <pegasucksgg@gmail.com>
2023-08-05 09:23:05 -0700

    bump

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs = {
     # nixpkgs.url = "github:pegasust/nixpkgs/staging-xcbuild-drv";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    boost.url = "git+https://git.pegasust.com/pegasust/nix-boost.git?ref=devshell-encapsulation";
+    boost.url = "git+https://git.pegasust.com/pegasust/nix-boost.git?ref=bleed";
   };
   outputs = inps@{self, boost, nixpkgs, ...}: 
     let inherit (boost.inputs) std;
